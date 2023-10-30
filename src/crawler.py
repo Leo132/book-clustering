eb4b855a408038ea235038dbc6cffa1348d4d5ee
leo2317 <adcb0981@gmail.com>
2023-10-30 22:31:44 +0800

    updated book info, including ./data/ and crawler.py

--- src/crawler.py
+++ src/crawler.py
@@ -2,9 +2,7 @@
 Web crawler tools lib
 
 Target websites:
-1. https://www.104.com.tw/jobs/search/?ro=0&expansionType=area%2Cspec%2Ccom%2Cjob%2Cwf%2Cwktm&indcat=1001000000&order=16&asc=0&sr=99&rostatus=1024&page=1&mode=s&langFlag=0&langStatus=0&recommendJob=1&hotJob=1 (104 人力銀行)
-2. https://www.truemovie.com/tairelease2022.htm (movie)
-3. https://www.sanmin.com.tw/promote/top/?id=yy&item=11209&pi=1 (book)
+    https://www.sanmin.com.tw/promote/top/?id=yy&item=11209&pi=1 (book)
 '''
 
 import requests
@@ -17,6 +15,8 @@ class Parser(BeautifulSoup):
     def __init__(self, markup: str, **kwargs):
         super().__init__(markup, "html.parser", **kwargs)
 
+
+# utils
 
 def get_html_from_url(url: str, method: str="get"):
     header = {
@@ -55,6 +55,7 @@ def save_info_to_json(page_urls: list[str], folder_file: str, get_urls: Callable
     print("Save success")
 
 
+''' ---------------- unused functions
 # 104 job bank
 
 def get_job_urls(url: str):
@@ -123,8 +124,9 @@ def _truemovie_test():
         for idx, movie_url in enumerate(get_movie_urls(movie_page_url)):
             movie_info = get_movie_info(movie_url)
             print(f"{idx:3d}. {movie_info}")
+'''
 
-# bookstore
+# crawl functions for bookstore
 
 def get_book_urls(url: str):
     result = Parser(get_html_from_url(url))
@@ -134,6 +136,18 @@ def get_book_urls(url: str):
 
     return book_urls
 
+'''
+Book info format
+    {
+        "name"      : name                  : str,
+        "author"    : author                : str (defalut: None),
+        "phouse"    : publishing house      : str (defalut: None),
+        "date"      : publication date      : str (defalut: None),
+        "category"  : category              : str (only one),
+        "price"     : price                 : int,
+        "pages"     : number of pages       : int (default: None),
+    }
+'''
 def get_book_info(url: str):
     html = get_html_from_url(url)
     if html is None:
@@ -147,8 +161,10 @@ def get_book_info(url: str):
     # book name
     book_name = div_tag.contents[1].string
 
-    # book pages
+    # info tag list for book pages, publication date, author and publishing house
     info_tag = div_tag.contents[3].find_all("li", attrs={"class": "mainText ga"})
+
+    # book pages
     book_pages = None
     for li_tag in info_tag:
         if "頁" in li_tag.text:
@@ -162,9 +178,22 @@ def get_book_info(url: str):
             book_date = li_tag.text[4:]
             break
 
+    # book author
+    book_author = None
+    for li_tag in info_tag:
+        if "作者" in li_tag.text:
+            book_author = li_tag.text[3:]
+            break
+
+    # book publishing house
+    book_phouse = None
+    for li_tag in info_tag:
+        if "出版社" in li_tag.text:
+            book_phouse = li_tag.text[4:]
+            break
+
     # book category
-    book_category_list = [] if len(div_tag.contents) < 6 else div_tag.contents[5].find_all('a', attrs={"class": "text-blue bold"})
-    book_category = None if len(book_category_list) == 0 else book_category_list[-1].text
+    book_category = result.find("div", attrs={"id": "breadcrumb-trail"}).contents[5].text
     
     # book price
     info_tag = result.find("div", attrs={"class": "mw300 lh-25 m0"}).contents[1]
@@ -173,21 +202,27 @@ def get_book_info(url: str):
             book_price = int(li_tag.text.replace(' ', '')[7:-1])
             break
 
-    info = {
+    return {
         "name": book_name,
-        "price": book_price,
+        "author": book_author,
+        "phouse": book_phouse,
         "category": book_category,
-        "pages": book_pages,
         "date": book_date,
+        "price": book_price,
+        "pages": book_pages,
     }
 
-    return info
+def get_author_info(url: str):
+    pass
+
+def get_phouse_info(url: str):
+    pass
 
 def save_book_to_json():
     book_page_urls = [f"https://www.sanmin.com.tw/promote/top/?id=yy&item=11209&pi={page + 1}" for page in range(25)]
     # skip those books that are out of print (e.g., page 17 - 334. <王室緋聞守則...>)
 
-    save_info_to_json(book_page_urls, "book_info_/book_info_page", get_book_urls, get_book_info)
+    save_info_to_json(book_page_urls, "book_info/book_info_page", get_book_urls, get_book_info)
 
 def _book_test():
     # book_page_urls = [f"https://www.sanmin.com.tw/promote/top/?id=yy&item=11209&pi={page + 1}" for page in range(25)]
@@ -199,30 +234,21 @@ def _book_test():
     #         book_info = get_book_info(book_url)
     #         print(f"{idx:3d}. {book_info}")
 
-    print(get_book_info("https://www.sanmin.com.tw/product/index/012289833"))
+    # print(get_book_info("https://www.sanmin.com.tw/product/index/010713721"))
+    print(get_author_info("https://www.sanmin.com.tw/search/index/?au=%e9%99%b3%e8%81%b0%e5%af%8c"))
 
 # testing
 
 def _test():
-    # info_list = get_contact_lense_info([f"./data/web/contact_lenses_page{idx + 1}.html" for idx in range(5)])
-    # info_list = get_motor_info([
-    #     "https://www.yamaha-motor.com.tw/motor/motorcycle.aspx",
-    #     # "https://www.yamaha-motor.com.tw/motor/scooter.aspx"
-    # ])
+    test_url = "https://www.sanmin.com.tw/search/index/?au=%e9%99%b3%e8%81%b0%e5%af%8c"
 
-    # for idx, info in enumerate(info_list):
-    #     print(f"{idx + 1:3d}. {info}")
-
-    test_url = "https://www.sanmin.com.tw/product/index/010203446"
-
-    _job_test()
+    # _job_test()
     # _truemovie_test()
     # _book_test()
     # save_book_to_json()
 
-    # with open("./src/view.html", 'wb') as f:
-    #     f.write(get_html_from_url(test_url, "get").encode("utf-8"))
-
+    with open("./src/view.html", 'wb') as f:
+        f.write(get_html_from_url(test_url, "get").encode("utf-8"))
 
 if __name__ == "__main__":
     _test()
--- src/test.py
+++ src/test.py
@@ -1,13 +1,1 @@
-import json
-
-data = {
-    'a': 1,
-    'h': 2,
-    'c': 3,
-    'd': 4,
-}
-x = [n for n in range(10)]
-y = [n*2 for n in range(10)]
-
-for xi, yi in zip(*[x, y]):
-    print(xi, yi)
+print("%e")
