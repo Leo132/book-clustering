2a6665abd2e40546e61856bcfddcf85b1594f81e
leo2317 <adcb0981@gmail.com>
2023-11-24 20:39:30 +0800

    test fastapi

--- src/main.py
+++ src/main.py
@@ -2,8 +2,24 @@
 Main application
 '''
 
-def main():
-    pass
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-if __name__ == "__main__":
-    main()
+_TITLE = "我的閱讀助手"
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/{page}", response_class=HTMLResponse)
+async def load_page(request: Request, page: str):
+    print(f"load {page}.html...")
+    kwargs = {
+        "request": request,
+        "title": _TITLE,
+    }
+    return templates.TemplateResponse(f"{page}.html", kwargs)
